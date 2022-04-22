7ee326510ad543d029a474ca45c510594ba7e7f7
harleyguru <harleyguru@outlook.com>
2022-04-22 21:15:31 +0300

    added debugging, adjusted for production, wrote readme

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # Stealth Backend
 
 Perform text anaylysis for incoming emails to the specified email address on IBM Watson NLP and store email content with emotion score into DynamoDB table
+
+## Prerequisites
+
+- Python3.8
+- Pipenv
+- NodeJS v14 over
+- Yarn
+- Serverless Framework
+- AWS CLI
+```
+$ aws configure --profile stealth # configure the credentials and region: us-east-1
+```
+- Configure Parameter Store
+
+Set `SENDER` and `RECIPIENT` parameters in AWS SSM's Parameter Store console before deploying the app below.
+
+These are environment variables for the app.
+
+## Install 
+
+Install dev dependencies
+```
+$ yarn # in root directory
+```
+
+Install dependencies for app
+```
+$ pipenv --three --site-packages # this creates venv for the project
+
+$ pipenv shell # get into the venv
+$ pipenv # install dependencies
+```
+
+## Deploy
+
+```
+$ sls deploy # This deployes the app to AWS
+```
+
+You might also want to deploy only updated function after you'd make some changes in `handler.py` file.
+In this case, you don't have to deploy the entire app as it will take much longer time.
+```
+$ sls deploy -f Analyze # This is very fast deployment
+```
+## Teardown
+```
+$ sls remove # This removes all deployed resources from AWS
+```
--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 import base64
+from decimal import Decimal
 import boto3
 import mailparser
 from watson_emotion_score import run
@@ -12,6 +13,17 @@ logger.setLevel(logging.INFO)
 s3 = boto3.resource('s3')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(f"Master-{os.environ['STAGE']}")
+
+
+def delete_email(message_id):
+    """
+    Delete email object from S3 bucket
+    """
+    # 1. Read email from S3 bucket
+    bucket_name = os.environ['BUCKET']
+    email_object = s3.Object(bucket_name, f'{message_id}')
+    email_object.delete()
+    logger.info('Deleted email object: %s from bucket', message_id)
 
 
 def extract_attachment_and_body(message_id):
@@ -28,7 +40,9 @@ def extract_attachment_and_body(message_id):
 
     # 2. Try to extract an attachment from email
     mail = mailparser.parse_from_bytes(body)
-    result = {'body': mail.body}
+    result = {}
+    if mail.text_plain and isinstance(mail.text_plain, list) and len(mail.text_plain) > 0:
+        result['body'] = mail.text_plain[0]
 
     if mail.attachments and len(mail.attachments) > 0:
         attachment = mail.attachments[0]
@@ -46,7 +60,7 @@ def extract_attachment_and_body(message_id):
         attachment_object.put(Body=payload)
         logger.info('Saved attachment file: %s in bucket', key)
 
-        result['key'] = key
+        result['attachment'] = key
 
     # 4. Delete email from S3 bucket
     email_object.delete()
@@ -71,8 +85,8 @@ def analyze(event, context):
         # 2. Extract email subject, body, sender, date
         sender = mail['source']
 
-        # if sender != 'dsam.marketing@gmail.com':
-        if sender != 'harleyguru@outlook.com':
+        if sender != os.environ['SENDER']:
+            delete_email(message_id)
             return
 
         subject = mail['commonHeaders']['subject']
@@ -83,42 +97,68 @@ def analyze(event, context):
         logger.info('Extracted necessary info from email: %s', message_id)
 
         # 4. Analyze email body using IBM Watson
-        score_dictionary = run(result['body'])
-        score = max(score_dictionary['sentiment']['positif'],
-                    score_dictionary['sentiment']['negatif'])
+        score = 0
+        if 'body' in result:
+            score_dictionary = run(result['body'])
+            score = max(score_dictionary['sentiment']['positif'],
+                        score_dictionary['sentiment']['negatif'])
 
         # 5. Save parsed email to DynamoDB table including emotion score
-        values = {
-            ':date': date,
-            ':subject': subject,
-            ':sender': sender,
-            ':body': result['body'],
-            ':score': score,
+        item = {
+            'date': date,
+            'subject': subject,
+            'sender': sender,
+            'body': result['body'],
+            'score': str(score),
         }
-        if result['attachment']:
-            values[':attachment'] = result['attachment']
+        if 'attachment' in result:
+            item['attachment'] = result['attachment']
+
+        item = json.loads(json.dumps(item), parse_float=Decimal)
+
+        logger.info('Processing result:')
+        logger.info(item)
 
         table.put_item(
-            Item={
-                '#date': ':date',
-                '#subject': ':subject',
-                '#sender': ':sender',
-                '#body': ':body',
-                '#attachment': ':attachment',
-                '#score': ':score',
-            },
-            ExpressionAttributeNames={
-                '#date': 'date',
-                '#subject': 'subject',
-                '#sender': 'sender',
-                '#body': 'body',
-                '#attachment': 'attachment',
-                '#score': 'score',
-            },
-            ExpressionAttributeValues=values
+            Item=item
         )
 
         logger.info('Successfully processed the email')
     except Exception as e:
         logger.error(e)
         logger.exception('Error')
+
+
+if __name__ == '__main__':
+    test_event = {
+        "Records": [
+            {
+                "eventSource": "aws:ses",
+                "eventVersion": "1.0",
+                "ses": {
+                    "mail": {
+                        "timestamp": "2022-04-22T17:32:51.765Z",
+                        "source": "harleyguru@outlook.com",
+                        "messageId": "qeg6875pkpance3umhhlgdbhu48aosffidhv3jo1",
+                        "destination": [
+                            "sam@networkerspace.com"
+                        ],
+                        "commonHeaders": {
+                            "returnPath": "harleyguru@outlook.com",
+                            "from": [
+                                "harley guru <harleyguru@outlook.com>"
+                            ],
+                            "date": "Fri, 22 Apr 2022 17:32:49 +0000",
+                            "to": [
+                                "\"sam@networkerspace.com\" <sam@networkerspace.com>"
+                            ],
+                            "messageId": "<BD8EB966-EDC9-4D86-8928-1D9C5F22AFC3@outlook.com>",
+                            "subject": "Re: Test subject"
+                        }
+                    },
+                }
+            }
+        ]
+    }
+
+    analyze(test_event, None)
