5a8d4ff0483c5e9bbfe015068b21e4dde3534361
harleyguru <harleyguru@outlook.com>
2022-04-22 19:40:32 +0300

    completed analyzer

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
+import os
 import json
 import logging
-from datetime import datetime
 import base64
 import boto3
 import mailparser
@@ -10,52 +10,115 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 s3 = boto3.resource('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(f"Master-{os.environ['STAGE']}")
 
-def extract_attachment(message_id):
+
+def extract_attachment_and_body(message_id):
     """
-    Extract an attachment from email in S3 bucket and store it back
+    Extract an attachment and body from the email in email bucket and store it attachment bucket
+
+    Returns key of attachment object and body
     """
     # 1. Read email from S3 bucket
-    email_object = s3.Object('irrcap-projects', f'portfolios/{message_id}')
+    bucket_name = os.environ['BUCKET']
+    email_object = s3.Object(bucket_name, f'{message_id}')
     body = email_object.get()['Body'].read()
     logger.info('Got read email object of messageId: %s from bucket', message_id)
 
-    # 2. Extract an attachment from email
+    # 2. Try to extract an attachment from email
     mail = mailparser.parse_from_bytes(body)
-    attachment = mail.attachments[0]
-    payload = attachment['payload']
-    payload = base64.b64decode(payload)
-    logger.info('Parsed email and extracted attachment')
+    result = {'body': mail.body}
 
-    # 3. Store an attachment to S3 bucket
-    attachment_object = s3.Object(
-        'irrcap-projects',
-        f"portfolios/{attachment['filename']}"
-    )
-    attachment_object.put(Body=payload)
-    logger.info('Saved attachment file in bucket')
+    if mail.attachments and len(mail.attachments) > 0:
+        attachment = mail.attachments[0]
+        payload = attachment['payload']
+        payload = base64.b64decode(payload)
+        logger.info('Parsed email and extracted attachment')
+
+        # 3. Store an attachment to S3 bucket
+        attachment_bucket = os.environ['ATTACHMENT_BUCKET']
+        key = f"{message_id}-{attachment['filename']}"
+        attachment_object = s3.Object(
+            attachment_bucket,
+            key
+        )
+        attachment_object.put(Body=payload)
+        logger.info('Saved attachment file: %s in bucket', key)
+
+        result['key'] = key
 
     # 4. Delete email from S3 bucket
     email_object.delete()
-    logger.info('Deleted email object from bucket')
+    logger.info('Deleted email object: %s from bucket', message_id)
 
-def analyze(event):
+    return result
+
+
+def analyze(event, context):
     """
     This handles incoming email
     """
     logger.info(json.dumps(event))
 
-    # 1. Get messageId to handle
-    record = event['Records'][0]
-    message_id = record['ses']['mail']['messageId']
-    logger.info('Handling email corresponding to messageId: %s', message_id)
+    try:
+        # 1. Get messageId to handle
+        record = event['Records'][0]
+        mail = record['ses']['mail']
+        message_id = mail['messageId']
+        logger.info('Handling email corresponding to messageId: %s', message_id)
 
-    # 2. Extract email subject, body, sender, date
+        # 2. Extract email subject, body, sender, date
+        sender = mail['source']
 
-    # 3. Extract attachment from email corresponding to the specific messageId
-    extract_attachment(message_id)
-    logger.info('Extracted attachment from email: %s', message_id)
+        # if sender != 'dsam.marketing@gmail.com':
+        if sender != 'harleyguru@outlook.com':
+            return
 
-    # 4. Analyze email body using IBM Watson
+        subject = mail['commonHeaders']['subject']
+        date = mail['timestamp']
 
-    # 5. Save parsed email to DynamoDB table including emotion score
+        # 3. Extract email attachment
+        result = extract_attachment_and_body(message_id)
+        logger.info('Extracted necessary info from email: %s', message_id)
+
+        # 4. Analyze email body using IBM Watson
+        score_dictionary = run(result['body'])
+        score = max(score_dictionary['sentiment']['positif'],
+                    score_dictionary['sentiment']['negatif'])
+
+        # 5. Save parsed email to DynamoDB table including emotion score
+        values = {
+            ':date': date,
+            ':subject': subject,
+            ':sender': sender,
+            ':body': result['body'],
+            ':score': score,
+        }
+        if result['attachment']:
+            values[':attachment'] = result['attachment']
+
+        table.put_item(
+            Item={
+                '#date': ':date',
+                '#subject': ':subject',
+                '#sender': ':sender',
+                '#body': ':body',
+                '#attachment': ':attachment',
+                '#score': ':score',
+            },
+            ExpressionAttributeNames={
+                '#date': 'date',
+                '#subject': 'subject',
+                '#sender': 'sender',
+                '#body': 'body',
+                '#attachment': 'attachment',
+                '#score': 'score',
+            },
+            ExpressionAttributeValues=values
+        )
+
+        logger.info('Successfully processed the email')
+    except Exception as e:
+        logger.error(e)
+        logger.exception('Error')
--- watson_emotion_score.py
+++ watson_emotion_score.py
@@ -6,24 +6,28 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
 
 
-ENTITIES = ["important", "urgent", "asap", "emergency", "unusual", "bug", "error", "compromised", "issue", "software", "voicemail", "quickly" ]
+ENTITIES = ["important", "urgent", "asap", "emergency", "unusual", "bug",
+            "error", "compromised", "issue", "software", "voicemail", "quickly"]
 NEG_EMOTIONS = ["anger", "disgust", "fear", "sadness"]
 POS_EMOTIONS = ["joy"]
 
 
-authenticator = IAMAuthenticator('iZfTKBZOO9GLYun6JEC2x5UyRX5GaHQP3vmHSgeTX-5m')
+authenticator = IAMAuthenticator(
+    'iZfTKBZOO9GLYun6JEC2x5UyRX5GaHQP3vmHSgeTX-5m')
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
     authenticator=authenticator
 )
-natural_language_understanding.set_service_url('https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/a8c4b690-e67e-406a-94cd-5fd94a0951ea')
+natural_language_understanding.set_service_url(
+    'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/a8c4b690-e67e-406a-94cd-5fd94a0951ea')
 
 
 def run(input_text):
 
     output = dict()
     # Get the NLU response on the targeted keywords
-    nlu_response = natural_language_understanding.analyze(text=input_text.lower(), features=Features(emotion=EmotionOptions(targets=ENTITIES))).get_result()
+    nlu_response = natural_language_understanding.analyze(text=input_text.lower(
+    ), features=Features(emotion=EmotionOptions(targets=ENTITIES))).get_result()
     # filter on the emotion
     emotions_dict = nlu_response["emotion"]
     # filter on the keywords emotions
@@ -31,16 +35,16 @@ def run(input_text):
     scores = list()
     # iterate on each keyword details in order to compute the overall scores
     for target_items in targeted_words_emotion:
-        emotion_target = target_items["emotion"]   
+        emotion_target = target_items["emotion"]
         sorted_scores = get_sorted_scores(emotion_target)
         main_emotions = get_main_emotions(sorted_scores)
         scores.append(main_emotions)
     overall = compute_overall_emotion(scores)
 
     # concatenate the output json file
-    output["text"] = input_text 
+    output["text"] = input_text
     output["sentiment"] = overall
-    output["detailed_sentiment"] =  targeted_words_emotion 
+    output["detailed_sentiment"] = targeted_words_emotion
     return output
 
 
@@ -55,35 +59,36 @@ def compute_overall_emotion(scores):
         for (key, value) in score_item.items():
             if key in POS_EMOTIONS:
                 positive += value
-                counter_positive +=1.0
+                counter_positive += 1.0
             elif key in NEG_EMOTIONS:
                 negative += value
-                counter_negatif +=1.0
-    if counter_negatif > 0.0 :
+                counter_negatif += 1.0
+    if counter_negatif > 0.0:
         negative = negative/counter_negatif
 
-    if counter_positive >0.0:
+    if counter_positive > 0.0:
         positive = positive/counter_positive
-    return {"positif":positive, "negatif": negative}
+    return {"positif": positive, "negatif": negative}
 
 
 def get_sorted_scores(emotion_target):
     return {k: v for k, v in sorted(emotion_target.items(), key=lambda item: item[1], reverse=True)}
 
-# function that filters on the emotions > 0.10 
+# function that filters on the emotions > 0.10
+
+
 def get_main_emotions(sorted_scores):
     main_emotions = dict()
     # Iterate over all the items in dictionary and filter items which has even keys
     for (key, value) in sorted_scores.items():
-    # Check if key is even then add pair to new dictionary
+        # Check if key is even then add pair to new dictionary
         if value >= 0.10:
             main_emotions[key] = value
     return main_emotions
 
 
-
-
 if __name__ == '__main__':
-    texts = ["Hi it’s Mal. We have discussed your mortgage account. There seem to be unusual activity on your account. Please call us or visit branch", "Hi. Can you call me quickly. It’s regarding your loan instalment.", "Hi. It’s Sam from IT support. We have fixed the urgent issues you had with your voicemail. Please contact us", "Hi there, this is important, call me! "]
+    texts = ["Hi it’s Mal. We have discussed your mortgage account. There seem to be unusual activity on your account. Please call us or visit branch", "Hi. Can you call me quickly. It’s regarding your loan instalment.",
+             "Hi. It’s Sam from IT support. We have fixed the urgent issues you had with your voicemail. Please contact us", "Hi there, this is important, call me! "]
     for input_text in texts:
         print(json.dumps(run(input_text), indent=2))
